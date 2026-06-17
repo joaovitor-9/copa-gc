1c36d8f7b66de4f5612a87610350717f4b2fa66a
joaovitor-9 <joaovamorim11@gmail.com>
2026-06-16 23:39:43 -0300

    feat(copa): adiciona registrarPartida e listarGrupo

--- src/service/Copa.java
+++ src/service/Copa.java
@@ -15,6 +15,19 @@ public class Copa {
         selecoes.add(s);
     }
 
+    public void registrarPartida(Selecao a, Selecao b, int golsA, int golsB) {
+        partidas.add(new Partida(a, b, golsA, golsB));
+    }
+
+    public void listarGrupo(char grupo) {
+        System.out.println("--- Grupo " + grupo + " ---");
+        for (Selecao s : selecoes) {
+            if (s.getGrupo() == grupo) {
+                System.out.println(s);
+            }
+        }
+    }
+
     public List<Selecao> getSelecoes() {
         return selecoes;
     }
